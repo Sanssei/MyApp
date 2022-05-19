159eacce2a65b60da40519fc1025d4061c25131c
Sanssei <a.s.borodin.1998@gmail.com>
2022-05-19 11:40:28 +0300

    fix bag_1

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     private val adapter = SmileAdapter()
     private val imageIdList = listOf(R.drawable.smile1, R.drawable.smile2, R.drawable.smile3,
         R.drawable.smile4, R.drawable.smile5, R.drawable.smile6, R.drawable.smile7,
-        R.drawable.smile8, R.drawable.smile9, R.drawable.smile10)
+        R.drawable.smile8)
     private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +22,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun init () {
         binding.apply{
-            recyclerView.layoutManager = GridLayoutManager(this@MainActivity, 2)
+            recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
             recyclerView.adapter = adapter
             buttonAdd.setOnClickListener {
-                if (index > 9) index = 0
+                if (index > 7) index = 0
                 val smile = Smile(imageIdList[index], "Smile $index")
                 adapter.addSmile(smile)
                 index++
